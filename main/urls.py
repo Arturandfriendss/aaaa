4b16365812db160ddc0299fc9e4b0c8600937ab9
Arturandfriendss <91111515+Arturandfriendss@users.noreply.github.com>
2021-10-05 17:16:42 +0300

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('about/', views.about, name="about"),
+    path('new_deal/', views.new_deal, name="new_deal")
+]
